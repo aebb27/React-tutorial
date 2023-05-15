2b3f4429a45c11bb6a1cddc6d4da7e4cb1aaa54f
aebb27 <69436407+aebb27@users.noreply.github.com>
2023-05-15 16:48:15 +0000

    Adjust the image size based on a prop
    Challenge 2: Adjust the image size based on a prop
    Link: https://react.dev/learn/passing-props-to-a-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,25 @@
-import './App.css'
-import { getImageUrl } from './utils.js'
+import { getImageUrl } from './utils.js';
 
-function Profile({ person, size = 70 }) {
+function Avatar({ person, size }) {
   return (
-    <section className="profile">
-      <h2>{person.name}</h2>
-      <img className="avatar" src={getImageUrl(person.imageId)} alt={person.name} width={size} height={size} />
-      <ul>
-        <li>
-          <b>Profession:</b> {person.profession}
-        </li>
-        <li>
-          <b>Awards: {person.awards.length} </b> ({person.awards.join(', ')})
-        </li>
-        <li>
-          <b>Discovered:</b> {person.discover}
-        </li>
-      </ul>
-    </section>
-  )
+    <img
+      className="avatar"
+      src={size < 90 ? getImageUrl(person, 's') : getImageUrl(person, 'b')}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
 }
 
-export default function Gallery() {
-  const people = [
-        {name: 'Maria Skłodowska-Curie', imageId:'szV5sdG', profession: 'physicist and chemist', awards: ['Nobel Prize in Physics', 'Nobel Prize in Chemistry', 'Davy Medal', 'Matteucci Medal'], discover: 'polonium (element)'}, 
-    {name: 'Katsuko Saruhashi', imageId: 'YfeOqp2', profession: 'geochemist', awards: ['Miyake Prize for geochemistry', 'Tanaka Prize'], discover: 'a method for measuring carbon dioxide in seawater'}
-  ]
-
+export default function Profile() {
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <Profile person={people[0]} />
-      <Profile person={people[1]} />
-    </div>
-  )
+    <Avatar
+      size={40}
+      person={{ 
+        name: 'Gregorio Y. Zara', 
+        imageId: '7vQD0fP'
+      }}
+    />
+  );
 }
