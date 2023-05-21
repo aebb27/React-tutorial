184c73b329bc71046a2b051bda25c5dbe0584824
aebb27 <69436407+aebb27@users.noreply.github.com>
2023-05-21 18:09:28 +0000

    Splitting a list in two
    Challenge 1: Splitting a list in two
    Link: https://react.dev/learn/rendering-lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
 import { people } from './data.js'
 import { getImageUrl } from './utils.js'
 
+function ListPeople ({ title, people }) {
+  return (
+    <>
+      <h2>{title}</h2>
+      <ul>
+        {people.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              know for {person.accomplishment}
+            </p>
+          </li>
+       )}
+      </ul>
+    </>
+  )
+}
 
 export default function List () {
-  const chemists = people.filter(person => person.profession === 'chemist')
-
-  const listItems = chemists.map(chemist => 
-      <li key={chemist.id}>
-        <img
-          src={getImageUrl(chemist)}
-          alt={chemist.name}
-        />
-        <p>
-          <b>{chemist.name}</b>
-          {' ' + chemist.profession + ' '}
-          known for {chemist.accomplishment}
-        </p>
-      </li>
+  const chemists = people.filter(person => person.profession === 'chemist'
+    )
+  
+  const everyone = people.filter(person => person.profession !== 'chemist'
   )
-  return <ul>{listItems}</ul> 
+  
+  return (
+    <>
+      <article>
+        <h1>Scientists</h1>
+        <hr/>
+        <ListPeople title="Chemists" people={chemists} />
+        <ListPeople title="Everyone else" people={everyone} />
+      </article>
+    </>
+  )
 }
