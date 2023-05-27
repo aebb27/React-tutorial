b9e7405097b6284d827ee03ba1a04b61b789262f
aebb27 <69436407+aebb27@users.noreply.github.com>
2023-05-27 09:28:05 +0000

    Refactor a series of ? : to if and variables
    Challenge 3: Refactor a series of ? : to if and variables
    Link: https://react.dev/learn/conditional-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-function Item({ name, importance }) {
-  return (<li>
-    {name} {importance > 0 && <i>(Importance: {importance})</i>}
-  </li>)
-}
-  
+const DRINKS = {
+    tea: {
+      part: 'leaf',
+      caffeine: '15-70 mg/cup',
+      age: '4,000+ years'
+    },
+    coffee: {
+      part: 'bean',
+      caffeine: '80-185 mg/cup',
+      age: '1,000+ years'
+    }
+  }
 
-export default function PackingList() {
+function Drink({ name }) {
+  const info = DRINKS[name]
   return (
     <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item importance={9} name="Space suit" />
-        <Item importance={0} name="Helmet with a golden leaf" />
-        <Item importance={6} name="Photo of Tam" />
-      </ul>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{info.part}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{info.caffeine}</dd>
+        <dt>Age</dt>
+        <dd>{info.age}</dd>
+      </dl>
     </section>
+  )
+}
+
+export default function DrinkList() {
+  return (
+    <div>
+      <Drink name="tea" />
+      <Drink name="coffee" />
+    </div>
   )
 }
