14d5c7d53e4bc8440135fce68ff2a85bc2bd0335
aebb27 <69436407+aebb27@users.noreply.github.com>
2023-05-29 03:17:47 +0000

    Fix stuck form inputs
    Challenge 2: Fix stuck inputs
    Link: https://react.dev/learn/state-a-components-memory

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,36 @@
 import { useState } from 'react'
-import { sculptureList } from './sculpture-data.js'
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0)
-  const [showMore, setShowMore] = useState(false)
-
-  let hasPrev = index > 0,
-    hasNext = index < sculptureList.length - 1
-
-  function next() {
-    if (hasNext) {
-      setIndex(index + 1)
-    }
+export default function Form() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value)
   }
 
-  function previous() {
-    if (hasPrev) {
-      setIndex(index - 1)
-    }
+  function handleLastNameChange(e) {
+    setLastName(e.target.value)
   }
 
-  function handleClick() {
-    setShowMore(!showMore)
+  function handleReset() {
+    setFirstName('')
+    setLastName('')
   }
-
-  let sculpture = sculptureList[index]
 
   return (
-    <>
-      <button onClick={previous}>Previous</button>
-      <button onClick={next}>Next</button>
-      <h2>
-        <i>{sculpture.name} </i> by {sculpture.artist}
-      </h2>
-      <h3>({index + 1} of {sculptureList.length})</h3>
-      <button onClick={handleClick}>
-        {showMore ? 'Hide' : 'Show'} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img
-        src={sculpture.url}
-        alt={sculpture.alt}
+    <form onSubmit={e => e.preventDefault()}>
+      <input
+        placeholder="First Name"
+        value={firstName}
+        onChange={handleFirstNameChange}
       />
-    </>
+      <input
+        placeholder="Last Name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>Hi, {firstName} {lastName}</h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
   )
 }
